4e925f0f36d38eac0a0595f73976c3557cac94af
thomasreichmann <thomasalmeidar@gmail.com>
2021-12-07 14:17:14 -0300

    added base firebase function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+export const generateToken = functions.https.onRequest((request, response) => {
+	functions.logger.info(`Logging request for token generetion ${request}`, { structuredData: true });
+
+	response.send(request.params);
+});
