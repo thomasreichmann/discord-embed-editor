d95d7f5e42c444670af05877484c0a131362f55f
Thomas <thomasreichmann@hotmail.com>
2021-12-09 00:43:04 -0300

    used env

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className="App">
+			<Button href={process.env.REACT_APP_DISCORD_OAUTH_URL}>Login with Discord</Button>
+		</div>
+	);
 }
 
 export default App;
