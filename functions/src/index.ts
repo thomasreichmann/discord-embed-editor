103b231b59b8ba89c5e6986fed3050f2b09b4c46
thomasreichmann <thomasalmeidar@gmail.com>
2021-12-17 02:12:22 -0300

    changed node fetch versions to work on functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,35 +10,36 @@ export const generateToken = functions.https.onCall(async (data, context) => {
 	let code = data.code;
 
 	try {
-		// const params = new url.URLSearchParams({ ...discord, code, grant_type: 'authorization_code' });
+		// const form = new FormData();
+		// form.append('client_id', discord.client_id);
+		// form.append('client_secret', discord.client_secret);
+		// form.append('redirect_uri', discord.redirect_uri);
+		// form.append('code', code);
+		// form.append('grant_type', 'authorization_code');
+		// let form = JSON.stringify({
+		// 	...discord,
+		// 	grant_type: 'authorization_code',
+		// 	code,
+		// });
+		let info = {
+			...discord,
+			grant_type: 'authorization_code',
+			code,
+		};
 
-		const form = new FormData();
-		form.append('client_id', discord.client_id);
-		form.append('client_secret', discord.client_secret);
-		form.append('redirect_uri', discord.redirect_uri);
-		form.append('code', code);
-		form.append('grant_type', 'authorization_code');
+		let params = new URLSearchParams(info);
 
 		let response = await fetch('https://discordapp.com/api/oauth2/token', {
 			method: 'POST',
-			body: data,
+			body: params.toString(),
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
 		});
 
-		// axios.post('https://example.com', form, { headers: form.getHeaders() });
+		let body: any = await response.json();
 
-		// let data = `client_id=${discord.client_id}&client_secret=${discord.client_secret}&redirect_uri=${discord.redirect_uri}&code=${code}&grant_type=authorization_code`;
-
-		// let data = { ...discord, code, grant_type: 'authorization_code' };
-
-		// const config = {
-		// 	headers: {
-		// 		'Content-Type': 'application/x-www-form-urlencoded',
-		// 	},
-		// };
-
-		// let response = await axios.post('https://discord.com/api/oauth2/token', JSON.stringify(data), config);
-
-		return `${functions.config().discord.client_secret}` || '';
+		return `${JSON.stringify(body)}` || '';
 	} catch (err) {
 		functions.logger.error(err);
 		throw new functions.https.HttpsError('internal', `discord responded with an error: ${err}`);
