8fb663f9610b0fd6561c5d3e716880b927c1eb7c
thomasreichmann <thomasalmeidar@gmail.com>
2021-12-20 21:02:58 -0300

    temp

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import * as url from 'url';
 import * as FormData from 'form-data';
 import fetch from 'node-fetch';
+import * as querystring from 'qs';
 
 export const generateToken = functions.https.onCall(async (data, context) => {
 	const discord = functions.config().discord;
@@ -10,30 +11,31 @@ export const generateToken = functions.https.onCall(async (data, context) => {
 	let code = data.code;
 
 	try {
-		// const form = new FormData();
-		// form.append('client_id', discord.client_id);
-		// form.append('client_secret', discord.client_secret);
-		// form.append('redirect_uri', discord.redirect_uri);
-		// form.append('code', code);
-		// form.append('grant_type', 'authorization_code');
+		//TODO: attempt to use qs to create formdata
+		const form = new FormData();
+		form.append('client_id', discord.client_id);
+		form.append('client_secret', discord.client_secret);
+		form.append('redirect_uri', discord.redirect_uri);
+		form.append('code', code);
+		form.append('grant_type', 'authorization_code');
 		// let form = JSON.stringify({
 		// 	...discord,
 		// 	grant_type: 'authorization_code',
 		// 	code,
 		// });
-		let info = {
-			...discord,
-			grant_type: 'authorization_code',
-			code,
-		};
+		// let info = {
+		// 	...discord,
+		// 	grant_type: 'authorization_code',
+		// 	code,
+		// };
 
-		let params = new URLSearchParams(info);
+		// let params = new URLSearchParams(info);
 
 		let response = await fetch('https://discordapp.com/api/oauth2/token', {
 			method: 'POST',
-			body: params.toString(),
+			body: form,
 			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Type': 'multipart/form-data',
 			},
 		});
 
